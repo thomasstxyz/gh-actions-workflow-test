aa32c2f3f3507f3754b00823f22b24f54f2de706
Thomas Stadler <thomas@thomasst.xyz>
2023-11-27 10:04:39 +0100

    feat: change tf

--- terraform/line-infra/main.tf
+++ terraform/line-infra/main.tf
@@ -1,4 +1,30 @@
+resource "random_string" "random_string" {
+  keepers = {
+    first = "${timestamp()}"
+  }     
+  length = 4
+  numeric = false
+  special = false
+  upper = false
+}
+
 resource "exoscale_iam_role" "my_role" {
-  name = "thomassttest1"
-  description = "mydescription"
+  name = "thomassttest321"
+  description = "Example role"
+  editable = true
+
+  policy = {
+    default_service_strategy = "deny"
+    services = {
+      sos = {
+        type = "allow"
+      }
+    }
+  }
+
+}
+
+resource "exoscale_iam_api_key" "my_api_key" {
+  name = "thomassttest321-${random_string.random_string.result}"
+  role_id = exoscale_iam_role.my_role.id
 }
